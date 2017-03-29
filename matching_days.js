baf68f96c47c945b58b480de0a98d43f9e8421b5
Lakhiwe <siyasangalakhiwe6@gmail.com>
2017-03-29 17:17:02 +0200

    added remove methods to change colors

--- matching_days.js
+++ matching_days.js
@@ -4,22 +4,28 @@ var date1 = document.querySelector('.box1');
 var date2 = document.querySelector('.box2');
 
 
+input.addEventListener('change', function() {
+    var dt1 = new Date(date1.value);
+    var dt2 = new Date(date2.value);
+    var firstDate = dt1.getDay();
+    var secDate = dt2.getDay();
+    for (var i = 0; i < daysArray.length; i++) {
+        daysArray[i].classList.remove('color3');
+        daysArray[i].classList.remove('color2');
+        daysArray[i].classList.remove('color1');
+    }
 
-console.log(date1.value);
+    if (firstDate === secDate) {
+        daysArray[secDate].classList.add('color3');
 
-input.addEventListener('change', function () {
-  var dt1 = new Date(date1.value);
-  var dt2 = new Date(date2.value);
-  var firstDate = dt1.getDay();
-  var secDate = dt2.getDay();
-  for (var i = 0; i < daysArray.length; i++) {
-    daysArray[i];
+    } else if (firstDate){
+
+      daysArray[firstDate].classList.add('color1');
+      daysArray[secDate].classList.add('color2');
+
+    {
+
+      daysArray[secDate].classList.add('color2');
   }
-if (firstDate === secDate) {
-  daysArray.classList.add('color3');
-}else {
-  daysArray[firstDate].classList.add('color1');
-  daysArray[secDate].classList.add('color2');
 }
-
 });
